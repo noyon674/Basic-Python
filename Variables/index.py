f085fcc9207ae06c69aea3c13af83c62cd9e96fd
Noyon sarker <124178299+noyon674@users.noreply.github.com>
2023-02-10 15:57:10 +0600

    Add files via upload

---
+++ Variables/index.py
@@ -0,0 +1,40 @@
+# Python Basic
+# variable declear
+x = 'Noyon Sarker'
+y = 20
+print(x,y) #output
+
+firstName = 'Noyon' #camel Case
+LastName = 'Sarker' #pascal Case
+father_name_is = 'Vanu sarker' #snake Case
+
+############ Many values to Multiple variable
+x,y,z = 10,20,50
+print(x)
+print(y)
+print(z)
+
+############## One value to Maltiple variable
+x=y=z = 100
+print(x,y,z)
+
+############### Unpack a collection
+fruit = ['apple', 'banana', 'mango'];
+x,y,z = fruit
+print(x)
+print(y)
+print(z)
+
+
+################ Python Built-In Data type
+x = 'Purna' #string
+x = 674 #int
+x = 35.674 #float
+x = 674j #complex
+x = ['a', 'b', 'c'] #list
+x = ('x', 'y', 'z') #tuple
+x = range(10) #range
+x = {'Name':'Noyon', 'Age':23} #dict
+x = {'Toyota', 'BMW', 'Honda'} #set
+x = True #Boolean
+
